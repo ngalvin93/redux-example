59032f36f1318daa36cf1b51bf4dfb7e62f2ce94
Alvin Ng <ngalvin93@gmail.com>
2019-11-04 23:48:45 -0600

    add and subtract 5 button

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ subscribe(() => render(getState()))
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+
+// Dispatch the "DECREMENT" action every time the -1 button is pressed
+const decrementButton = document.getElementById('decrement')
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }))
+
+const incrementBy5Button = document.getElementById('incrementBy5')
+incrementBy5Button.addEventListener('click', e => dispatch({ type: "INCREMENT_BY_5"}))
+
+const decrementBy5Button = document.getElementById('decrementBy5')
+decrementBy5Button.addEventListener('click', e => dispatch({ type: "DECREMENT_BY_5"}))
--- src/reducer.js
+++ src/reducer.js
@@ -3,10 +3,26 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    let { type } = action;
+    let { type } = action; // not sure what this is doing...
     if (type === "INCREMENT") {
         return {
             value: state.value + 1
+        }
+    } else if (type === "DECREMENT") {
+        return {
+            value: state.value - 1
+        }
+    } else if (type === "INCREMENT_BY_5") {
+        return {
+            value: state.value + 5
+        }
+    } else if (type === "DECREMENT_BY_5") {
+        return {
+            value: state.value - 5
+        }
+    } else if (type === "DECREMENT_BY_5") {
+        return {
+            value: state.value - 5
         }
     }
     return state
